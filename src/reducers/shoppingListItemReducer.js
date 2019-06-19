401812345caea59c557150ce4088f1d928c81f3f
OctopiWithHats <sdgranoff@gmail.com>
2019-06-19 14:56:44 -0400

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,25 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
-	switch (action.type) {
-		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+//reducer is just producing a counter
+//instead of having all functions encapsulated in closure within index.js, we've now separated out the reducer function, given it a relevant name, and let Redux library take care of createStore function
+//these two pieces are both imported into src/index.js and used to create store
+//store value then passed as prop to Provider
 
-		default:
-			return state;
-	}
+export default function shoppingListItemReducer(
+  state = {
+    items: []
+  },
+  action
+) {
+  console.log(action);
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      console.log('Current state.items length %s', state.items.length);
+      console.log('Updating state.items length to %s', state.items.length + 1);
+      return Object.assign({}, state, {
+        items: state.items.concat(state.items.length + 1)
+      });
+
+    default:
+      console.log('Initial state.items length: %s', state.items.length);
+      return state;
+  }
 }
